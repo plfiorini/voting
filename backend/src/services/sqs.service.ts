7adccd3498974360a168b0eead97ec1d7ed581b4
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2023-03-27 11:11:55 +0200

    backend: AWS SDK v3

--- backend/src/services/sqs.service.ts
+++ backend/src/services/sqs.service.ts
@@ -1,4 +1,4 @@
-import AWS from 'aws-sdk';
+import  { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import config from '../config';
 
 interface IMessage {
@@ -10,19 +10,18 @@ interface IPublishOptions {
 }
 
 export async function publishToSQS(message: IMessage, options: IPublishOptions): Promise<void> {
-    const sqs = new AWS.SQS({
-        endpoint: config.aws.endpoint ? new AWS.Endpoint(config.aws.endpoint) : undefined,
+    const sqs = new SQSClient({
+        endpoint: config.aws.endpoint,
         region: config.aws.region,
         credentials: {
             accessKeyId: config.aws.accessKeyId,
             secretAccessKey: config.aws.secretAccessKey,
         },
     });
-    console.debug(`Connecting to ${sqs.endpoint.href} ...`);
     console.debug(`Queue: ${options.queueUrl}`);
 
-    await sqs.sendMessage({
+    await sqs.send(new SendMessageCommand({
         QueueUrl: options.queueUrl,
         MessageBody: JSON.stringify(message),
-    }).promise();
+    }));
 }
