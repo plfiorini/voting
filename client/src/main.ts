506efe7fa64d89f00e0b74957896b8499b6ef5bd
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2023-04-05 08:30:17 +0200

    client: Parametri da riga di comando

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,27 +1,42 @@
+import { parseArgs } from 'util';
 import { AxiosResponse } from 'axios';
 import { Poll, PollClient } from './polls';
 
-const pollClient = new PollClient('http://localhost:8080');
+const defaultUrl = 'http://localhost:8080';
 
-const pollQuestions: Record<string, string[]> = {
-    'What is your favorite color?': ['Red', 'Blue', 'Green', 'Yellow', 'Other'],
-    'How often do you exercise?': ['Every day', 'A few times a week', 'Once a week', 'Less than once a week', 'Never'],
-    'What is your favorite food?': ['Pizza', 'Sushi', 'Burgers', 'Tacos', 'Other'],
-    'How many hours of sleep do you get per night?': ['8 or more hours', '6-8 hours', '4-6 hours', 'Less than 4 hours', 'It varies'],
-    'What is your favorite hobby?': ['Reading', 'Playing sports', 'Watching movies/TV shows', 'Traveling', 'Other'],
-    'What is your favorite type of music?': ['Rock', 'Pop', 'Hip hop', 'Country', 'Other'],
-    'How do you prefer to spend your weekends?': ['Relaxing at home', 'Hanging out with friends', 'Exploring new places', 'Doing outdoor activities', 'Other'],
-    'What is your favorite season?': ['Spring', 'Summer', 'Fall', 'Winter', 'Other'],
-    'What is your favorite movie genre?': ['Action', 'Comedy', 'Drama', 'Sci-fi', 'Other'],
-    'What is your favorite type of pet?': ['Dog', 'Cat', 'Fish', 'Bird', 'Other']
-};
-
-let polls: Poll[] = Object.entries(pollQuestions).map(([question, options]) => ({
-    question,
-    options
-}));
+const {
+    values: { url },
+} = parseArgs({
+    options: {
+        url: {
+            type: 'string',
+            short: 'u',
+            default: defaultUrl,
+        },
+    },
+});
 
 async function run() {
+    const pollClient = new PollClient(url ? url : defaultUrl);
+
+    const pollQuestions: Record<string, string[]> = {
+        'What is your favorite color?': ['Red', 'Blue', 'Green', 'Yellow', 'Other'],
+        'How often do you exercise?': ['Every day', 'A few times a week', 'Once a week', 'Less than once a week', 'Never'],
+        'What is your favorite food?': ['Pizza', 'Sushi', 'Burgers', 'Tacos', 'Other'],
+        'How many hours of sleep do you get per night?': ['8 or more hours', '6-8 hours', '4-6 hours', 'Less than 4 hours', 'It varies'],
+        'What is your favorite hobby?': ['Reading', 'Playing sports', 'Watching movies/TV shows', 'Traveling', 'Other'],
+        'What is your favorite type of music?': ['Rock', 'Pop', 'Hip hop', 'Country', 'Other'],
+        'How do you prefer to spend your weekends?': ['Relaxing at home', 'Hanging out with friends', 'Exploring new places', 'Doing outdoor activities', 'Other'],
+        'What is your favorite season?': ['Spring', 'Summer', 'Fall', 'Winter', 'Other'],
+        'What is your favorite movie genre?': ['Action', 'Comedy', 'Drama', 'Sci-fi', 'Other'],
+        'What is your favorite type of pet?': ['Dog', 'Cat', 'Fish', 'Bird', 'Other']
+    };
+
+    let polls: Poll[] = Object.entries(pollQuestions).map(([question, options]) => ({
+        question,
+        options
+    }));
+
     // Delete all polls
     await pollClient.getPolls().then((response: AxiosResponse<Poll[]>) => {
         response.data.forEach(async (poll) => {
